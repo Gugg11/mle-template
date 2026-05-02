8b75af2680875a036f53cadd9f337200d2ef3603
Bulat Nasybullin <bulat-nasybullin@mail.ru>
2026-05-02 21:52:53 +0300

    Lab 3: подключение к PostgreSQL через Vault

--- src/db.py
+++ src/db.py
@@ -1,18 +1,24 @@
-import os
 import psycopg2
+from vault_client import get_postgres_secret, write_postgres_secret
 
 
 def get_connection():
+    # получаем секреты из Vault
+    secret = get_postgres_secret()
+
     return psycopg2.connect(
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST"),
-        port=os.getenv("POSTGRES_PORT"),
+        dbname=secret["POSTGRES_DB"],
+        user=secret["POSTGRES_USER"],
+        password=secret["POSTGRES_PASSWORD"],
+        host=secret["POSTGRES_HOST"],
+        port=secret["POSTGRES_PORT"],
     )
 
 
 def init_db():
+    # сначала записываем секреты в Vault (один раз)
+    write_postgres_secret()
+
     conn = get_connection()
     cur = conn.cursor()
 
