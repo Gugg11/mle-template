59e41b56abbdb8dedb99bdaae222cda2ecf16856
Bulat Nasybullin <bulat-nasybullin@mail.ru>
2026-05-11 00:44:09 +0300

    Код обучения и тестов: убраны лишние модели, исправлен predict

--- src/unit_tests/test_training.py
+++ src/unit_tests/test_training.py
@@ -1,7 +1,6 @@
 import configparser
 import os
 import unittest
-import pandas as pd
 import sys
 
 sys.path.insert(1, os.path.join(os.getcwd(), "src"))
@@ -19,21 +18,6 @@ class TestMultiModel(unittest.TestCase):
 
     def test_log_reg(self):
         self.assertEqual(self.multi_model.log_reg(), True)
-
-    def test_rand_forest(self):
-        self.assertEqual(self.multi_model.rand_forest(use_config=False), True)
-
-    def test_knn(self):
-        self.assertEqual(self.multi_model.knn(use_config=False), True)
-
-    def test_svm(self):
-        self.assertEqual(self.multi_model.svm(use_config=False), True)
-
-    def test_gnb(self):
-        self.assertEqual(self.multi_model.gnb(), True)
-
-    def test_d_tree(self):
-        self.assertEqual(self.multi_model.d_tree(use_config=False), True)
 
 
 if __name__ == "__main__":
